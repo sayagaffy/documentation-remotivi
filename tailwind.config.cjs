e6edbc1212f3526011e7484d8f0cf00943811525
sayagaffy <rgaffyagb@gmail.com>
2023-03-07 00:17:00 +0700

    update responsive try 1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      
+    },
   },
   plugins: [],
 };
