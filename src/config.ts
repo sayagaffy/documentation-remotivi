ba0550b5bfb4ef251565c03b652707ee7010fdc0
sayagaffy <rgaffyagb@gmail.com>
2023-03-07 12:54:37 +0700

    adding full page documentation

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,36 @@ export const SIDEBAR = [
   { text: "Content Editor", link: "/core/editor-content" },
   { text: "Research", link: "/core/research" },
   { text: "Event", link: "/core/event" },
-  
+  { text: "Project", link: "/core/project" },
+  { text: "Video", link: "/core/video" },
+  { text: "Bangku Belakang", link: "/core/bangku-belakang" },
 
+  { text: "Categories", header: true },
+  { text: "Tag sistem", link: "/core/tag-sistem" },
+  { text: "Rubrik", link: "/core/rubrik" },
+  
+  { text: "Banner", header: true },
+  { text: "Page", link: "/core/page-banner" },
+  { text: "Project", link: "/core/project-banner" },
+
+  { text: "Member", header: true },
+  { text: "Member", link: "/core/member" },
+
+  { text: "Admin", header: true },
+  { text: "Admin", link: "/core/admin" },
+
+  { text: "About Us", header: true },
+  { text: "Profile", link: "/core/Profile" },
+  { text: "Career", link: "/core/Career" },
+  { text: "Partners", link: "/core/Partners" },
+
+  { text: "Annual Report", header: true },
+  { text: "Annual Report", link: "/core/annual-report" },
+
+  { text: "Complaint", header: true },
+  { text: "Complaint", link: "/core/complaint" },
+
+  { text: "dev-scripts", header: true },
+  { text: "dev-scripts", link: "/core/dev-scripts" },
   
 ];
---
+++ src/pages/core/tag-sistem.md
@@ -0,0 +1,24 @@
+---
+title: Tag sistem
+description: penjelasan fitur Tag sistem
+layout: ../../layouts/MainLayout.astro
+---
+
+# Penjelasan Fitur Tag sistem
+
+Fitur Tag sistem memiliki banyak kesamaan dengan Fitur **Artikel** maka dari itu kami mendokumentasikan/merangkum perbedaan nya saja.
+
+### Fitur Upload PDF EN | ID
+
+- Field File Berfungsi untuk mengupload PDF EN | ID
+ 
+<img src="https://i.im.ge/2023/03/06/7PW37W.image.png">
+
+- Field Author Berfungsi untuk memasukan nama Peneliti.
+
+<img src="https://i.im.ge/2023/03/06/7PWYC0.image.png">
+
+- Hanya ada Field Description yang berfungsi menjadi lead dan digunakan juga untuk SEO, dan juga tidak lupa field EN | ID. 
+  
+<img src="https://i.im.ge/2023/03/06/7PAu2y.image.png">
+  
